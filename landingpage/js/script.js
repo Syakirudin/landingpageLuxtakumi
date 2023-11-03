44320da79fea6e0619a763050ea1fb19c9b60b91
Syakirudin <36721931+Syakirudin@users.noreply.github.com>
2023-11-03 21:38:24 +0800

    others functions

--- landingpage/js/script.js
+++ landingpage/js/script.js
@@ -11,5 +11,35 @@ document.addEventListener("mousemove", function(e) {
 });
 
 
+document.addEventListener("DOMContentLoaded", function() {
+    const navLinks = document.querySelectorAll(".nav-links a");
+
+    // Function to handle smooth scrolling
+    function smoothScroll(target) {
+        const offset = 60; // Adjust this value as needed
+        window.scrollTo({
+            top: target.offsetTop - offset,
+            behavior: "smooth"
+        });
+    }
+
+    // Add click event listeners to each navigation link
+    navLinks.forEach(function(link) {
+        link.addEventListener("click", function(event) {
+            event.preventDefault(); // Prevent default link behavior
+
+            const targetClass = this.getAttribute("data-target"); // Get the data-target attribute
+
+            const targetSection = document.querySelector(`.section[data-id="${targetClass}"]`);
+
+            if (targetSection) {
+                smoothScroll(targetSection);
+            }
+        });
+    });
+});
+
+
+
 
 
